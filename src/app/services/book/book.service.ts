2299c027f31e030917e681890cf5a8a1958c72f9
angelo lorensi <angelomozza@gmail.com>
2023-10-18 18:15:00 -0300

    deploy test 3

--- src/app/services/book/book.service.ts
+++ src/app/services/book/book.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Book } from 'src/app/shared/model/Book';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
